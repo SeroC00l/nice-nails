16790c4ccd0adb574ab558797ed73d33ef38d0aa
sero <aleurrea4@gmail.com>
2024-03-15 13:23:03 -0500

    yeah

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import db from "@astrojs/db";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), tailwind(), db()]
+});
