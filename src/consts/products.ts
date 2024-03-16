da2cd8780c37c1a9aab5f036324f2817cd7a6e89
sero <aleurrea4@gmail.com>
2024-03-16 16:47:54 -0500

    16/03

---
+++ src/consts/products.ts
@@ -0,0 +1,12 @@
+export const products= [
+    { id : "locion-facial",
+      name : "Loción Facial",
+      price : 50000,
+      description : "Lorem Ipsum ese textar de las industria, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum"
+     },
+     { id : "crema-manos",
+      name : "Crema para las manos",
+      price : 50000,
+      description : "Lorem Ipsum ese textar de las industria, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum"
+     }
+]
