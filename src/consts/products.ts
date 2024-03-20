b27c733f761cfd1a1ca780669f5bcbde9474342f
sero <aleurrea4@gmail.com>
2024-03-20 14:27:11 -0500

    20/03

--- src/consts/products.ts
+++ src/consts/products.ts
@@ -1,12 +1,64 @@
-export const products= [
-    { id : "locion-facial",
-      name : "Loción Facial",
-      price : 50000,
-      description : "Lorem Ipsum ese textar de las industria, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum"
-     },
-     { id : "crema-manos",
-      name : "Crema para las manos",
-      price : 50000,
-      description : "Lorem Ipsum ese textar de las industria, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum"
-     }
-]
+export const products = [
+  {
+    id: "mascarilla-facial-de-limpieza-profunda-clear-proof",
+    name: "Mascarilla facial de limpieza profunda con carbón Clear Proof® Mary Kay®",
+    price: 0,
+    description: "",
+    link: "",
+  },
+  {
+    id: "locion-humectante-efecto-reparador",
+    name: "Loción Humectante TimeWise® Efecto Renovador Mary Kay®",
+    price: 0,
+    description: "",
+    link: "",
+  },
+  {
+    id: "beige-n180-mate",
+    name: "Base TimeWise 3D® Beige W 180 – Mate",
+    price: 63000,
+    description:
+      "Formulada con el Complejo Age Minimize 3D®, la Base TimeWise 3D® Mate va más allá de ofrecer una cobertura impecable, ¡ofrece belleza sin edad! Además, fue creada con la Tecnología Intellimatch® que capturó los tonos reales de miles de mujeres alrededor del mundo. Los resultados fueron la guía para crear cada uno de los tonos para que cada mujer pueda encontrar el tono de base que combine perfectamente con su tono de piel.",
+    link: "",
+  },
+  {
+    id: "berry",
+    name: "Labial Líquido Mate Mary Kay At Play® Berry Strong",
+    price: 34000,
+    description:
+      "Impacta con un tono brillante y fuerte. Este tono te permitirá crear todas las combinaciones que puedas imaginar y dejará tus labios con un increíble acabado mate que te encantará.",
+    link: "",
+  },
+  {
+    id: "Espuma-Limpiadora-Facial-TimeWise-Repair",
+    name: "Espuma Limpiadora Facial TimeWise® Repair® Volu Firm® Mary Kay®",
+    price: 0,
+    description:
+      "Esta espuma rica en humectación limpia profundamente y renueva la textura de la piel, y le da a los poros una apariencia menos notable      Mantiene el balance de humectación.",
+    link: "",
+  },
+  {
+    id: "maqliq_luminoso_330",
+    name: "Maquillaje Líquido Acabado Luminoso TimeWise™ Beige 8",
+    price: 45900,
+    description:
+      "Gracias a su fórmula con extractos de Jojoba, la piel se ve más firme, joven y saludable al instante. Reduce visiblemente la apariencia de los poros, líneas finas, arrugas e imperfecciones para una piel luminosa.",
+    link: "",
+  },
+  {
+    id: "s-l1600",
+    name: "Mary Kay MK Men Daily Facial Wash",
+    price: 0,
+    description:
+      "",
+    link: "",
+  },
+  {
+    id: "locion-facial-regeneradora",
+    name: "Loción Facial Regeneradora C + E TimeWise®",
+    price: 117900,
+    description: "La Loción Facial Regeneradora C + E TimeWise® incluye la combinación extraordinaria de las vitaminas C + E. La vitamina E, es un antioxidante reconocido, que al mezclarse con la vitamina C, ayudan a defender la piel de los radicales libres que provocan el envejecimiento prematuro.",
+    link: "",
+  },
+  
+];
---
+++ src/consts/services.ts
@@ -0,0 +1,26 @@
+export const services = [
+  {
+    title: 'Manicure Semipermanente',
+    description: 'La manicura semipermanente es un método para el esmaltado de uñas ideal para aquellas personas que están siempre ocupadas, 21 días de uñas perfectas con Nice Nails.',
+    image: '/semipermanente.jpeg',
+    alt: 'foto de manicura semipermanente',
+  },
+  {
+    title: 'Manicure Acrílico',
+    description: 'Son aquellas que se realizan desde cero con acrílico en polvo (polímero) y acrílico líquido (montonero) ideal para aquellas con uñas débiles quebradizas y o mordidas. 21 días de uñas perfectas con Nice Nails!!',
+    image: '/acrilico.png',
+    alt: 'foto de manicura acrilica',
+  },
+  {
+    title: 'Manicure Tradicional',
+    description: 'El manicure tradicional es un servicio clásico que incluye la limpieza y cuidado de las uñas, la cutícula y un esmaltado regular. Perfecto para quienes prefieren un estilo sencillo y elegante.',
+    image: '/tradicional.jpeg',
+    alt: 'foto de manicura tradicional',
+  },
+  {
+    title: 'Pedicure',
+    description: 'El pedicure es un tratamiento estético que se realiza en los pies para cuidar la piel, las uñas y relajar los músculos. Disfruta de pies perfectamente cuidados y elegantes con Nice Nails.',
+    image: '/pedicure.png',
+    alt: 'foto de pedicura',
+  },
+];
