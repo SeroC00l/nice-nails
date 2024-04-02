d6f5f77b3807b990684bbaad32fc27e7ea32abab
sero <aleurrea4@gmail.com>
2024-04-01 21:12:27 -0500

    01/04

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			backgroundImage: {
-				'spa': "url('../../spa.jpg')",
-			  }
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        spa: "url('../../spa.jpg')",
+      },
+      colors: {
+        "primary": "#f54e85",
+      },
+    },
+  },
+  plugins: [],
+};
