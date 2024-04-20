4882f8c63007b280b53c212aeac9e6e88b642144
sero <aleurrea4@gmail.com>
2024-04-20 12:13:29 -0500

    vercel again

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import db from "@astrojs/db";
 
-import cloudflare from "@astrojs/cloudflare";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), db()],
   output: "server",
-  adapter: cloudflare(),
-  vite: {
-    ssr: {
-      noExternal: ["astro","@astrojs/image"],
-    },
-  },
+  adapter: vercel()
 });
