8bc63cd7684607811272105183d85d2fb23b7aa7
sero <aleurrea4@gmail.com>
2024-05-13 09:49:45 -0500

    implement price in cop

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import db from "@astrojs/db";
 import vercel from "@astrojs/vercel/serverless";
-
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
